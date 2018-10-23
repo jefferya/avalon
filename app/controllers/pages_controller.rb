e3dad0feb41d9884f6a10bf7e68613e9e5b7c9f1
Jeffery Antoniuk <jefferya@ualberta.ca>
2018-10-23 13:38:38 -0600

    add UAL footer content

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+
+  def about_eraav
+  end
+
+  def policies
+  end
+
+  def technology
+  end
+
+  def deposit
+  end
+  
+  def contact
+  end
+
+  def flash_help
+  end
+end
+
