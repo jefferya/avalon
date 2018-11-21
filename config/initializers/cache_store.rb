01469c6ccac389dd1ae5a432abc2e2431a7f94f1
Jeffery Antoniuk <jeffery.antoniuk@ualberta.ca>
2018-11-20 18:41:09 -0700

    tweak redis param

--- config/initializers/cache_store.rb
+++ config/initializers/cache_store.rb
@@ -3,7 +3,7 @@ if ["development", "test"].include?(Rails.env)
   config.cache_store = :memory_store, { size: 64.megabytes }
 else
   config.cache_store = :redis_store, {
-    url: Settings.redis_url,
+    url: Settings.redis.url,
     namespace: 'avalon'
   }
 end
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,7 +1,7 @@
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-Resque.redis = ENV['REDIS_URL'] || "#{Settings.redis.host}:#{Settings.redis.p  ort}"
+Resque.redis = ENV['REDIS_URL'] || "#{Settings.redis.host}:#{Settings.redis.port}"
 
 Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
 Resque.logger.level = Logger::INFO
